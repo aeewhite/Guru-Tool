c8af73b6e3441b8cad0fed8947df36d17bdac0c7
Andrew White <aeewhite@gmail.com>
2013-03-08 22:12:12 -0500

    Fixed the overflow issue, actually this time

--- js/boom.js
+++ js/boom.js
@@ -2,6 +2,8 @@ $('.bigdaddybutton').click(function () {
 	$('.flames').fadeIn();
 	$('.booooom').fadeIn();
 	$(".booooom").animate({
-	    "text-indent": "1800px"
-  }, 3250 );
+		"text-indent": "1800px"
+  }, 3250, function (){
+		 $('.booooom').hide();
+  });
 });
