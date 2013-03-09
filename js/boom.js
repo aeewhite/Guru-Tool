0969b2d4e00959774f6c4d99537d725f7208bca8
Andrew White <aeewhite@gmail.com>
2013-03-08 23:01:53 -0500

    Changed behavior of text t slide

--- js/boom.js
+++ js/boom.js
@@ -2,8 +2,9 @@ $('.bigdaddybutton').click(function () {
 	$('.flames').fadeIn();
 	$('.booooom').fadeIn();
 	$(".booooom").animate({
-		"text-indent": "-2500px"
+		"text-indent": "-3000px"
   }, 3250, function (){
 		 $('.booooom').hide();
+		 $('.booooom').css('text-indent','2000px')
   });
 });
