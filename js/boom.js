4a797fa83bc7f728fed915bfeed0919e53919c33
Andrew White <aeewhite@gmail.com>
2013-03-08 22:57:56 -0500

    I think it is right now, it better be

--- js/boom.js
+++ js/boom.js
@@ -2,7 +2,7 @@ $('.bigdaddybutton').click(function () {
 	$('.flames').fadeIn();
 	$('.booooom').fadeIn();
 	$(".booooom").animate({
-		"text-indent": "1800px"
+		"text-indent": "-2500px"
   }, 3250, function (){
 		 $('.booooom').hide();
   });
